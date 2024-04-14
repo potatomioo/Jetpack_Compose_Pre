00f8d2e810670ff4a5a953210e56a71274e8a946
Deeptanshu Shukla <108982699+x-deeptanshu@users.noreply.github.com>
2024-04-15 03:56:02 +0530

    dependency checked

--- app/src/main/java/com/example/jetpack_compose_setup/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_setup/MainActivity.kt
@@ -4,68 +4,28 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.BottomSheetScaffoldState
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Label
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Snackbar
-import androidx.compose.material3.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.jetpack_compose_setup.ui.theme.Jetpack_compose_setupTheme
 import org.w3c.dom.Text
-import kotlin.random.Random
-import androidx.compose.runtime.*
-import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.time.delay
+
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -359,12 +319,14 @@ class MainActivity : ComponentActivity() {
 fun maketextfield(
     given:String
 ){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(color = Color.White),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.White), Arrangement.Center, Alignment.CenterHorizontally
     ) {
+
+        Text(text = "heyy")
+
 
 
         var textstate = remember {
@@ -374,7 +336,7 @@ fun maketextfield(
             modifier = Modifier
             .border(
                 width = 5.dp,
-                brush = Brush.horizontalGradient(listOf(Color.Blue, Color.Black)),
+                brush = Brush.horizontalGradient(listOf(Color.Blue, Color.Black), startX = 0.5f),
                 shape = RoundedCornerShape(5.dp)
             ),
             value = textstate.value,
@@ -386,8 +348,20 @@ fun maketextfield(
 //            },
             textStyle = TextStyle(
                 color = Color.Black
-            )
-            )
+            ),
+            leadingIcon = {
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "name"
+                ,modifier = Modifier
+                        .padding(0.dp)
+                        .background(
+                            color = Color.Black,
+                            shape = RoundedCornerShape(5.dp)
+                        )
+                    ,tint = Color.White
+                )
+            }
+        )
+
     }
 }
 
